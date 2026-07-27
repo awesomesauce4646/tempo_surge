8e8082f1bb8bff9dc6aaee1678b46919b0592a47
awesomesauce4646 <mary_grellmann@yahoo.com>
2026-07-27 06:08:08 -0500

    exporting, moving files around

--- detect_click.gd
+++
@@ -1,7 +0,0 @@
-extends TextureButton
-
-@onready var parent = $".."
-
-func _on_pressed() -> void: #YOU NEED TO CONNECT THIS SIGNAL FROM THE TAB NEXT TO INSPECTOR!!
-	hide()
-	parent.buttons_pressed += 1
